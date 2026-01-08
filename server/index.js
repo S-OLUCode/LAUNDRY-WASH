5957d2c6b5af48de2594094b460f46256a3fd2d8
Sayo Olumide <sayowayne7@gmail.com>
2026-01-08 06:37:23 -0800

    commit

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const httpServer = createServer(app);
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://laundrywash-client-vercel.app"],
+    origin: ["http://localhost:3000", "https://laundrywash-client-nine.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"], //
     optionsSuccessStatus: 200, //default status code
